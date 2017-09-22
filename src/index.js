3fddea13d9fab584bf698f83cd19ef1ab4f58a67
Matthew <wattmills@gmail.com>
2017-09-22 12:21:55 -0400

    added styling

--- src/index.js
+++ src/index.js
@@ -11,19 +11,31 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null 
+    };
 
-    YTSearch({key: API_KEY, term: 'pizza dog'}, (videos) => {
-      this.setState({ videos });
+    this.videoSearch('yung lean');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
     return (
     <div>
-      <SearchBar />
-      <VideoDetail />
-      <VideoList videos={this.state.videos} />
+      <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList 
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
     </div>
     ///
     );
